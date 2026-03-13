7519a331ceac506c8fcadcc76f31b50fcaf9f1de
Natali Joselin Alemán Perez <natali_1321124050@uptecamac.edu.mx>
2026-03-13 10:50:48 -0600

    fix: It overwrites the card file when restarting reads; it does not generate a new one

--- C_PY/PYTHONCS/cs.py
+++ C_PY/PYTHONCS/cs.py
@@ -1,40 +1,30 @@
 import serial
 import json
 
-# Conexión con el ESP32 (cambia COM4 si tu puerto es diferente)
 ser = serial.Serial('COM4', 115200)
 
-# Lista donde se guardarán los UID leídos
 lista_uid = []
 
 print("Esperando tarjetas RFID...")
 
 while True:
     try:
-        # Leer datos enviados por el ESP32
         linea = ser.readline().decode().strip()
 
         if linea:
-            # Convertir el JSON recibido a diccionario
             data = json.loads(linea)
-
-            # Obtener UID de la tarjeta
             uid = data["uid"]
 
-            print("Tarjeta detectada:", uid)
+            print("Tarjeta RFID detectada:", uid)
 
-            # Verificar que el UID no esté ya en la lista
             if uid not in lista_uid:
                 lista_uid.append(uid)
 
-                # Guardar la lista en un archivo
-                with open("tarjetas.txt", "w") as archivo:
-                    for tarjeta in lista_uid:
-                        archivo.write(tarjeta + "\n")
+                # guardar sin borrar el archivo
+                with open("tarjetas.txt", "a") as archivo:
+                    archivo.write(uid + "\n")
 
-            # Mostrar lista actual
-            print("Lista de tarjetas:", lista_uid)
+            print("Lista de tarjetas RFID:", lista_uid)
 
     except:
-        # Ignorar errores y continuar
         pass
